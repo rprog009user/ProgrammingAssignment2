3e6b9853ef639753cf6c5b2c9d926bf4cdd59762
Anton Olenev <olenev@gmail.com>
2014-11-23 01:07:49 +0300

    ADD: comments added for functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions allow to store in cache value of inverted matrix
+## instead of calculate it each time. Their implementation based on 
+## sample functions which cache mean value of vector.
 
-## Write a short comment describing this function
+## Put inverted matrix into cache. That cached value can be retrived
+## later.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Get inverted matrix. Cache will be used if inverted matrix
+## has been already calculated before.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
